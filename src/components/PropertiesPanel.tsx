8753a76b61349aa4e550b2ee0e450fd05add3007
Marc Mariné <shenobi@gmail.com>
2025-06-15 23:26:13 +0200

    Add regulation link to properties panel

--- src/components/PropertiesPanel.tsx
+++ src/components/PropertiesPanel.tsx
@@ -1,4 +1,3 @@
-import { TemplateName } from 'energy-label'
 import { REGULATIONS } from '../lib/constants'
 import { withResizableSidebar, type ResizableSidebarProps } from '../lib/resizable-sidebar'
 import { useEnergyLabelStore } from '../lib/useEnergyLabelStore'
@@ -7,11 +6,11 @@ import DynamicInputList from './InputList'
 function PropertiesPanel(_: ResizableSidebarProps) {
   const { download, setData, template, data } = useEnergyLabelStore()
 
-  const templateTitle = REGULATIONS[template!].name
+  const { name: templateTitle, regulationNumber } = REGULATIONS[template!]
 
   return (
     <div class="panel relative flex-1 w-full">
-      <div class="p-1 mb-1 flex gap-2 items-center justify-between">
+      <div class="px-1 pt-1 flex gap-2 items-center justify-between">
         <h2 class="px-2 font-semibold truncate">{templateTitle}</h2>
         <div class="p-1 flex gap-0.5 rounded">
           <button class="button bg-neutral-100 dark:bg-slate-700/40">
@@ -30,6 +29,25 @@ function PropertiesPanel(_: ResizableSidebarProps) {
           </button>
         </div>
       </div>
+      {regulationNumber && (
+        <p class="px-3 mb-2 text-xs text-neutral-500 dark:text-slate-600">
+          <a href={`http://data.europa.eu/eli/reg_del/${regulationNumber}/oj`} class="hover:underline flex items-center gap-1" target="_blank">
+            Regulation (EU) {regulationNumber}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-3">
+              <path
+                fillRule="evenodd"
+                d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </p>
+      )}
       <div class="px-3 py-2">
         <DynamicInputList template={template!} values={data!} setValues={setData} />
       </div>
